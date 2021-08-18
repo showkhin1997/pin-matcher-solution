1e8fc0a7fc3c0428afb36725500d95a7443abe30
showkhin1997 <showkhin15@gmail.com>
2021-08-18 14:42:29 +0600

    verification

--- js/app.js
+++ js/app.js
@@ -28,4 +28,20 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
         const newNumber = previousNumber + number;
         calcInput.value = newNumber;
     }
-})
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typeNumber = document.getElementById('type-numbers').value;
+    const verifySuccess = document.getElementById('notify-success');
+    const verifyError = document.getElementById('notify-faield');
+    if (pin == typeNumber) {
+        verifySuccess.style.display = 'block';
+        verifyError.style.display = 'none';
+    }
+    else {
+        const verifyError = document.getElementById('notify-faield');
+        verifyError.style.display = 'block';
+        verifySuccess.style.display = 'none';
+    }
+}
