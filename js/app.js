11890963ebd70453ed912b920bdb892245f5749e
showkhin1997 <showkhin15@gmail.com>
2021-08-18 14:20:33 +0600

    numberpad

--- js/app.js
+++ js/app.js
@@ -13,3 +13,19 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('type-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
